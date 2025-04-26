9f7ec86dd65bb66d1307c18dbad091ae4ea9b1a1
colleunmax <mr.collejoulian@gmail.com>
2025-04-26 11:20:57 +0200

    feat: adding a setup for git in my install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+function fn_main() {
+    source $DOTFILES_DIR/scripts/setup/update.sh
+    echo "Installing you dotfiles"
+
+    source $DOTFILES_DIR/scripts/setup/dnf-setup.sh
+    source $DOTFILES_DIR/scripts/setup/symlink-dotfiles.sh
+    source $DOTFILES_DIR/scripts/setup/git-setup.sh
+}
+
+
+if [ $EUID -eq "0" ]; then
+    # fn_update_deps
+    fn_main
+else
+    exec sudo bash "$0" "$@"
+fi
---
+++ scripts/setup/git-setup.sh
@@ -0,0 +1,16 @@
+dnf install -y git
+
+if gum confirm "Would you like to update your Git username and email?"; then
+  IS_RUNNING=true
+  while $IS_RUNNING; do
+    GIT_USERNAME=$(gum input --placeholder "Enter your Git username")
+    GIT_EMAIL=$(gum input --placeholder "Enter your Git email")
+
+    if gum confirm "Are these correct? Username: $GIT_USERNAME, Email: $GIT_EMAIL"; then
+      IS_RUNNING=false
+    fi
+  done
+  su -c "git config --global user.name "$GIT_USERNAME"" $SUDO_USER
+  su -c "git config --global user.email "$GIT_EMAIL"" $SUDO_USER
+  exit
+fi
