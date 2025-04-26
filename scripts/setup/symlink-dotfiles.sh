a1cb4fc64f6d6c9ce8777e42a29c9dabc4f356ed
colleunmax <mr.collejoulian@gmail.com>
2025-04-26 13:18:31 +0200

    feat: adding symlinks for fonts and scripts

--- scripts/setup/symlink-dotfiles.sh
+++ scripts/setup/symlink-dotfiles.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 ln -sf $DOTFILES_DIR/configs/.config/* ~/.config
 ln -sf $DOTFILES_DIR/configs/home/* ~/
+
+# SWWW Scripts
+ln -sf $DOTFILES_DIR/scripts/swww/* /usr/bin
+
+# Flameshot Script
+ln -sf $DOTFILES_DIR/scripts/screenshot.sh
+
+# Fonts
+mkdir -p ~/.local/share/fonts
+ln -df $DOTFILES_DIR/fonts/* ~/.local/share/fonts
