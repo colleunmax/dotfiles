3b068a683c9750499471a2741e43fc610f2e28a3
colleunmax <mr.collejoulian@gmail.com>
2025-04-27 12:10:04 +0200

    fix: bug with the file permissions and user permissions in general

--- scripts/setup/symlink-dotfiles.sh
+++ scripts/setup/symlink-dotfiles.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-ln -sf $DOTFILES_DIR/configs/.config/* ~/.config
-ln -sf $DOTFILES_DIR/configs/.config/.* ~/.config
-ln -sf $DOTFILES_DIR/configs/home/* ~/
-ln -sf $DOTFILES_DIR/configs/home/.* ~/
+
+chmod +x $DOTFILES_DIR/scripts/*
+
+su -c "ln -sf $DOTFILES_DIR/configs/.config/* ~/.config" $SUDO_USER
+su -c "ln -sf $DOTFILES_DIR/configs/.config/.* ~/.config" $SUDO_USER
+su -c "ln -sf $DOTFILES_DIR/configs/home/* ~/" $SUDO_USER
+su -c "ln -sf $DOTFILES_DIR/configs/home/.* ~/" $SUDO_USER
 
 # SWWW Scripts
-ln -sf $DOTFILES_DIR/scripts/swww/* /usr/bin
+ln -sf $DOTFILES_DIR/scripts/swww/change_wallpaper.sh /usr/bin/change_wallpaper
+ln -sf $DOTFILES_DIR/scripts/swww/setup_wallpaper.sh /usr/bin/setup_wallpapers
 
 # Flameshot Script
-ln -sf $DOTFILES_DIR/scripts/screenshot.sh
+ln -sf $DOTFILES_DIR/scripts/screenshot.sh /usr/bin/screenshot
 
 # Fonts
 mkdir -p ~/.local/share/fonts
-ln -df $DOTFILES_DIR/fonts/* ~/.local/share/fonts
+su -c "ln -sf $DOTFILES_DIR/fonts/* ~/.local/share/fonts" $SUDO_USER
