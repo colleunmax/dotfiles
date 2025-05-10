4c50b51cd35f9c9d2b9a04e5deb9d4979baf4ee2
colleunmax <mr.collejoulian@gmail.com>
2025-05-10 15:21:58 +0200

    feat: adding wlogout and swww to install script

--- scripts/setup/system-app.sh
+++ scripts/setup/system-app.sh
@@ -1,5 +1,19 @@
-dnf install -y hyprland hyprlock waybar wofi alacritty flameshot
+dnf install -y hyprland hyprlock waybar wofi alacritty flameshot wlogout lz4 lz4-devel
 
 # zsh
 dnf install -y zsh zsh-autosuggestions zsh-syntax-highlighting
 chsh -s /usr/bin/zsh $SUDO_USER
+
+# wlogout
+ln -sf $DOTFILES_DIR/images/wlogout/* /usr/share/wlogout/icons
+
+# swww
+git clone https://github.com/LGFae/swww.git ~/swww
+cd ~/swww
+cargo build --release
+./doc/gen.sh
+cp ~/swww/doc/generated/* /usr/local/share/man/man1
+cp ~/swww/target/release/swww /usr/bin/swww
+cp ~/swww/target/release/swww-daemon /usr/bin/swww-daemon
+chmod 711 /usr/bin/swww
+chmod 711 /usr/bin/swww-daemon
