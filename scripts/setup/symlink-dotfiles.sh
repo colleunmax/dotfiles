5ec6502ee6b4a2ba83e7be768608d599190e7fab
colleunmax <mr.collejoulian@gmail.com>
2025-05-10 21:25:03 +0200

    fix: bug where wallpapers scripts wasn't recognised or didn't have the right permissions

--- scripts/setup/symlink-dotfiles.sh
+++ scripts/setup/symlink-dotfiles.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-chmod +x $DOTFILES_DIR/scripts/*
+chmod 775 -R $DOTFILES_DIR/scripts
 
 su -c "ln -sf $DOTFILES_DIR/configs/.config/* ~/.config" $SUDO_USER
 su -c "ln -sf $DOTFILES_DIR/configs/home/.* ~/" $SUDO_USER
 
 # SWWW Scripts
 ln -sf $DOTFILES_DIR/scripts/swww/change_wallpaper.sh /usr/bin/change_wallpaper
-ln -sf $DOTFILES_DIR/scripts/swww/setup_wallpaper.sh /usr/bin/setup_wallpapers
+ln -sf $DOTFILES_DIR/scripts/swww/setup_wallpapers.sh /usr/bin/setup_wallpapers
+su -c "sh /usr/bin/setup_wallpapers" $SUDO_USER
 
 # Flameshot Script
 ln -sf $DOTFILES_DIR/scripts/screenshot.sh /usr/bin/screenshot
